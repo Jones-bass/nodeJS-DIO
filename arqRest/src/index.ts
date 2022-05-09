e8b1d2fd419ad3f7f4f907555b14953383542cd2
jones-bass <jonesbass.tb@gmail.com>
2022-05-08 23:29:55 -0300

    Rota POST e config UUID

--- arqRest/src/index.ts
+++ arqRest/src/index.ts
@@ -3,12 +3,17 @@ import usersRoute from "./routes/usersRoute";
 
 const app = express();
 
+// Configuração da app - Convertendo o JSON
+app.use(express.json());
+
+// Configuração da Rota
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'OLA BB' })
 });
 
+// Inicialiação do Servidor
 app.listen(3000, () => {
   console.log('Node execuntando...!')
 })
--- arqRest/src/routes/usersRoute.ts
+++ arqRest/src/routes/usersRoute.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction, Router } from "express";
-
+import statusCode from 'http-status-codes';
 
 
 const usersRoute =  Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Jones' }];
-  res.status(200).send({ users })
+  res.status(statusCode.OK).send({ users })
+})
+
+usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(statusCode.OK).send({ uuid })
+})
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUsers = req.body;
+  res.status(statusCode.CREATED).send(newUsers);
 })
 
 export default usersRoute;
