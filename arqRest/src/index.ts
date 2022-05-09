89cb83a10a310ec41fcd8216f44d0facade15bdf
jones-bass <jonesbass.tb@gmail.com>
2022-05-08 21:13:44 -0300

    Escutando a porta3000

--- arqRest/src/index.ts
+++ arqRest/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Ola Mundo");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'BAR' })
+});
+
+app.listen(3000, () => {
+  console.log('Node execuntando...!')
+})
